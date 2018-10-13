1b29a43adad68b560de35381e98ed758c3d8da54
victorjaquez <victorajaquez@gmail.com>
2018-10-13 12:05:46 -0700

    Initial setup, api test

---
+++ client/src/components/Test.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Test",
+  data() {
+    return {
+      msg: ""
+    };
+  },
+  methods: {
+    getMessage() {
+      const path =
+        "https://0zrpjen2ze.execute-api.us-west-2.amazonaws.com/dev/test";
+      axios
+        .get(path)
+        .then(response => {
+          console.log(response.data);
+          this.msg = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  },
+  created() {
+    this.getMessage();
+  }
+};
+</script>
